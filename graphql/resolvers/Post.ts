5336007b40c7f00fdc4eb65d0a90c9c6f77fe47d
Arthur DELORT <arthurdelort@hotmail.fr>
2021-05-29 21:32:49 +0200

    Auto generate resolvers for crud  operations

--- graphql/resolvers/Post.ts
+++ graphql/resolvers/Post.ts
@@ -1,66 +1,31 @@
-import { extendType, intArg, nonNull, objectType, stringArg } from "nexus";
+import { extendType, objectType } from "nexus";
 
 export const Post = objectType({
-  name: "Post", // <- Name of your type
+  name: "Post",
   definition(t) {
-    t.int("id"); // <- Field named `id` of type `Int`
-    t.string("title"); // <- Field named `title` of type `String`
-    t.string("body"); // <- Field named `body` of type `String`
-    t.boolean("published"); // <- Field named `published` of type `Boolean`
+    t.model.id();
+    t.model.title();
+    t.model.body();
+    t.model.published();
   },
 });
 
 export const PostQuery = extendType({
   type: "Query",
   definition(t) {
-    t.list.field("drafts", {
-      type: "Post",
-      resolve(_root, _args, ctx) {
-        return ctx.db.post.findMany({ where: { published: false } });
-      },
-    });
-    t.list.field("posts", {
-      type: "Post",
-      resolve(_root, _args, ctx) {
-        return ctx.db.post.findMany({ where: { published: true } });
-      },
-    });
+    t.crud.post();
+    t.crud.posts({ filtering: true });
   },
 });
 
 export const PostMutation = extendType({
   type: "Mutation",
   definition(t) {
-    t.field("createDraft", {
-      type: "Post",
-      args: {
-        title: nonNull(stringArg()),
-        body: nonNull(stringArg()),
-      },
-      resolve(_root, args, ctx) {
-        const draft = {
-          title: args.title,
-          body: args.body,
-          published: false,
-        };
-        return ctx.db.post.create({ data: draft });
-      },
-    });
-    t.field("publish", {
-      type: "Post",
-      args: {
-        draftId: nonNull(intArg()),
-      },
-      resolve(_root, args, ctx) {
-        return ctx.db.post.update({
-          where: {
-            id: args.draftId,
-          },
-          data: {
-            published: true,
-          },
-        });
-      },
-    });
+    t.crud.createOnePost();
+    t.crud.deleteManyPost();
+    t.crud.deleteOnePost();
+    t.crud.updateManyPost();
+    t.crud.updateOnePost();
+    t.crud.upsertOnePost();
   },
 });
--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,5 +1,7 @@
 import { makeSchema } from "nexus";
+import { nexusPrisma } from "nexus-plugin-prisma";
 import { join } from "path";
+import { db } from "./db";
 import * as types from "./resolvers";
 
 export const schema = makeSchema({
@@ -8,4 +10,10 @@ export const schema = makeSchema({
     typegen: join(__dirname, "../generated/nexus-typegen.ts"),
     schema: join(__dirname, "../generated/schema.graphql"),
   },
+  plugins: [
+    nexusPrisma({
+      experimentalCRUD: true,
+      prismaClient: (ctx) => (ctx.prisma = db),
+    }),
+  ],
 });
